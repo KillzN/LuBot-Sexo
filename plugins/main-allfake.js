ad99a2e75f10840f2cfacd944ea59ee57fad9dfc
KillzN <scorpionsheep203@gmail.com>
2025-07-16 15:15:00 -0400

    LU-BOT

--- plugins/main-allfake.js
+++ plugins/main-allfake.js
@@ -33,9 +33,9 @@ handler.all = async function (m) {
   //let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
 
   //CREADOR Y OTROS
-  global.creador = 'Wa.me/56983073328'
+  global.creador = 'Wa.me/543704518876'
   global.botreal = `${(conn.user.jid == global.conn.user.jid ? '*Bot:* Oficial' : `*SubBot de: ${conn.user.jid.split('@')[0]}*`)}`
-  global.asistencia = 'Wa.me/56983073328'
+  global.asistencia = 'Wa.me/543704518876'
 
   //REACCIONES 
   global.rwait = '🕒'
